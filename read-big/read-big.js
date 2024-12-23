f4ce18cc1762881ecdfe81781ddc65f329917ef4
itsUtkarshOjha <utkarshojhalu@gmail.com>
2024-12-23 20:17:23 +0530

    read part begin

---
+++ read-big/read-big.js
@@ -0,0 +1,18 @@
+const fs = require("fs/promises");
+
+(async () => {
+  const fileHandleRead = await fs.open("src.txt", "r");
+  const fileHandleWrite = await fs.open("dest.txt", "w");
+
+  const streamRead = fileHandleRead.createReadStream();
+  const streamWrite = fileHandleWrite.createWriteStream();
+  streamRead.on("data", (chunk) => {
+    if (!streamWrite.write(chunk)) {
+      streamRead.pause();
+    }
+  });
+
+  streamWrite.on("drain", () => {
+    streamRead.resume();
+  });
+})();
